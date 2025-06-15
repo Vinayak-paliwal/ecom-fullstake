6881a5161cc8d169fe5c3473e8ec3cfa66ccb4cd
Vinayak-paliwal <vinayakpaliwal866@gmail.com>
2025-06-15 15:33:37 +0530

    yoo

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: "https://ecom-fullstake-3ysn.vercel.app",
+  credentials: true, 
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
